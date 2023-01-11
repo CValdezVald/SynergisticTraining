65a15c47466b8f75bed3cceb8e055293b848fb24
BuildTools <unconfigured@null.spigotmc.org>
2023-01-11 08:35:07 -0800

    Understanding ES6 and functions

--- FirstFile.js
+++ FirstFile.js
@@ -1,6 +1,17 @@
-//ctrl + -
-//REPL - read evaluate print and loop
-for (let index = 0; index < 100; index++) {
-    console.log(index);
-    
+//Core javascript vanilla js ES6
+//variable declaration
+
+var sessionName = "MERNStack Session", month = "January"
+console.log("session name is - ", sessionName+" "+ month)
+console.log("testing")
+
+
+//function creation in js
+function PrintSession(sessionName) {
+    console.log("session Name from function- ",sessionName)
 }
+
+
+//calling the function using var and manual input
+PrintSession(sessionName)
+PrintSession("Core Javascript")
